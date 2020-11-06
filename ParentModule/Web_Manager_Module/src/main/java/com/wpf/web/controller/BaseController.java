800f7c3f1dc2ecd0c407a89d31d44936d6c09d05
wpf <w1p2f3@126.com>
2020-11-06 17:07:16 +0800

    抽取出公用的BaseController控制器类

---
+++ ParentModule/Web_Manager_Module/src/main/java/com/wpf/web/controller/BaseController.java
@@ -0,0 +1,36 @@
+package com.wpf.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 创建时间：2020/11/6
+ * 从所有控制器中抽取出一部分共有的属性和方法
+ * @author wpf
+ */
+@Controller
+public abstract class BaseController {
+
+    @Autowired
+    HttpServletRequest request;
+
+    @Autowired
+    HttpServletResponse response;
+
+    @Autowired
+    HttpSession session;
+
+    public String getCompanyId() {
+        //目前先写死，完成登录功能后从用户数据中获取
+        return "1";
+    }
+
+    public String getCompanyName() {
+        //目前先写死，完成登录功能后从用户数据中获取
+        return "传智播客教育股份有限公司";
+    }
+}
--- ParentModule/Web_Manager_Module/src/main/java/com/wpf/web/controller/system/DepartmentController.java
+++ ParentModule/Web_Manager_Module/src/main/java/com/wpf/web/controller/system/DepartmentController.java
@@ -3,6 +3,7 @@ package com.wpf.web.controller.system;
 import com.github.pagehelper.PageInfo;
 import com.wpf.domain.system.Department;
 import com.wpf.service.system.DepartmentService;
+import com.wpf.web.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,7 +23,7 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/system/dept")
-public class DepartmentController {
+public class DepartmentController extends BaseController {
 
     @Autowired
     private DepartmentService departmentService;
@@ -32,7 +33,7 @@ public class DepartmentController {
                     @RequestParam(defaultValue = "5") Integer pageSize,
                     @RequestParam(defaultValue = "1") Integer pageNum) {
         //需要获取当用户所属的公司Id，目前先写死
-        String companyId = "1";
+        String companyId = getCompanyId();
         PageInfo<Department> pageInfo = departmentService.findDeptByPage(pageSize, pageNum, companyId);
         model.addAttribute("pageInfo", pageInfo);
         return "system/dept/dept-list";
@@ -46,7 +47,7 @@ public class DepartmentController {
     @RequestMapping("/toUpdate")
     public String jumpToUpdatePage(Model model, String id) {
         //需要获取当用户所属的公司Id，目前先写死
-        String companyId = "1";
+        String companyId = getCompanyId();
         //查询到需要回显的数据
         Department department = departmentService.findDeptById(id);
         //查询到下拉列表的数据
@@ -78,8 +79,8 @@ public class DepartmentController {
     @RequestMapping("/edit")
     public String editDepartment(Department department) {
         //需要获取当用户所属的公司Id和公司名，目前先写死
-        department.setCompanyId("1");
-        department.setCompanyName("传智播客教育股份有限公司");
+        department.setCompanyId(getCompanyId());
+        department.setCompanyName(getCompanyName());
         if (StringUtils.isEmpty(department.getDeptId())) {
             departmentService.addOneDepartment(department);
         } else {
